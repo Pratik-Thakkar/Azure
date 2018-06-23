9468fa76e8b39d5a3be136a84fbbc6580ef58f35
Pratik Thakkar <thakkarpratik277@gmail.com>
2018-06-23 07:29:51 +0530

    Update BackupCopytoAzureFiles-E30C.ps1

--- BackupCopytoAzureFiles-E30C.ps1
+++ BackupCopytoAzureFiles-E30C.ps1
@@ -4,7 +4,7 @@
 	Set-Alias Az $AzCopy
 	
 ## create azure files
-	$AzFiles = "https://xxxxx.file.core.windows.net/backup-e30c/Backup Job E30CUKDMS01"
+	$AzFiles = "https://xxxxx.file.core.windows.net/backup-e30c/Backup Job E30C"
 	$SgKey = ""
 
 	$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
@@ -15,7 +15,7 @@
 	echo ""
 	
 ##  client archive directory
-    $Source = "E:\Backup\Backup Job E30CUKDMS01"
+    $Source = ""
 
 ## copy archived zip files to azure files with AzCopy
 	echo ""	
